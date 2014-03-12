34a5391598a313e45d66c34c2d5616fd4306df09
Wei Lu <luwei.here@gmail.com>
2014-03-12 15:19:09 +0800

    disallow private node as root

--- app/services/hd_wallet.rb
+++ app/services/hd_wallet.rb
@@ -1,10 +1,20 @@
 class HDWallet
+  class RootNodeError < StandardError
+    def initialize message
+      super message << " For security, only a base58 serialized public node stripped of private key is allowed."
+    end
+  end
   attr_reader :root_node
 
   def initialize(public_base58)
     root_node = MoneyTree::Node.from_serialized_address(public_base58)
-    if(root_node.private_key.present?)
-      raise 'Private node detected. Only base58 serialized public node is allowed for security reasons.'
+
+    error_messages = []
+    error_messages << "Private key detected." if(root_node.private_key.present?)
+    error_messages << "Private node detected." if(root_node.is_private?)
+
+    if(error_messages.present?)
+      raise RootNodeError.new(error_messages.join(' '))
     end
     @root_node = root_node
   end
@@ -13,3 +23,4 @@ class HDWallet
     root_node.subnode(i).to_address
   end
 end
+
