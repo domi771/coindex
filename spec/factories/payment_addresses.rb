d54af8ef1ceb0a5b933666372f590a537817766b
Wei Lu <luwei.here@gmail.com>
2014-03-13 11:40:09 +0800

    payment address factory generates more realistic data

--- spec/factories/payment_addresses.rb
+++ spec/factories/payment_addresses.rb
@@ -1,7 +1,11 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
+  sequence :address_index
+
   factory :payment_address do
-    address "MyString"
+    address { Faker::Bitcoin.address }
+    currency { Currency.codes[:btc] }
+    address_index
   end
 end
