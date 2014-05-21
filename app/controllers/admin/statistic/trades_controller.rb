fbc0cacc45126270d12dacf81ad16273855063cb
shooterman <shooter.hero@gmail.com>
2014-05-21 18:46:30 +0800

    trades statistic

--- app/controllers/admin/statistic/trades_controller.rb
+++ app/controllers/admin/statistic/trades_controller.rb
@@ -1,24 +1,36 @@
 module Admin
   module Statistic
     class TradesController < BaseController
+
+      START_TIME = Time.now.beginning_of_day
+      END_TIME  = Time.now
+      TODAY_TRADE_RESULT = Trade.search(created_at_gteq: START_TIME, created_at_lteq: END_TIME).result
+
       def show
-        @trades_grid = ::Statistic::TradesGrid.new(params[:statistic_trades_grid])
-        @assets = @trades_grid.assets
+        @q = Trade.search(params[:q])
 
-        @groups = {
-          :volume => @assets.sum(:volume),
-          :amount => @assets.sum {|t| t.price * t.volume},
-          :avg_price => @assets.average(:price),
-          :max_price => @assets.maximum(:price),
-          :min_price => @assets.minimum(:price)
-        }
+        @count = @q.result.count
+        @volume_count = @q.result.sum(:volume)
 
-        @groups.merge!({
-          :volume_fee => (@groups[:volume]),
-          :amount_fee => (@groups[:amount]),
-          :count => @assets.all.size
-        })
+        @total_price = @q.result.sum("(volume * price)")
+
+        @today_trade_count = today_trade_count
+        @today_coin_count  = today_coin_count
+        @today_total_price = today_total_price
       end
+
+      private
+        def today_trade_count
+          TODAY_TRADE_RESULT.count
+        end
+
+        def today_coin_count
+          TODAY_TRADE_RESULT.sum(:volume)
+        end
+
+        def today_total_price
+          TODAY_TRADE_RESULT.sum("(volume * price)")
+        end
     end
   end
 end
