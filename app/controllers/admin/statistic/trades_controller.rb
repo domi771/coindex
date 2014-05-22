c93d2ef40d2309cb7569e0c1816874fc098af543
shooterman <shooter.hero@gmail.com>
2014-05-22 12:07:45 +0800

    trend statistic finshed

--- app/controllers/admin/statistic/trades_controller.rb
+++ app/controllers/admin/statistic/trades_controller.rb
@@ -1,35 +1,46 @@
 module Admin
   module Statistic
     class TradesController < BaseController
-
-      START_TIME = Time.now.beginning_of_day
-      END_TIME  = Time.now
-      TODAY_TRADE_RESULT = Trade.search(created_at_gteq: START_TIME, created_at_lteq: END_TIME).result
-
       def show
         @q = Trade.search(params[:q])
 
         @count = @q.result.count
         @volume_count = @q.result.sum(:volume)
-
         @total_price = @q.result.sum("(volume * price)")
+        @trade_members_count = (@q.result.pluck("DISTINCT(ask_member_id)") | @q.result.pluck("DISTINCT(bid_member_id)")).count
 
         @today_trade_count = today_trade_count
         @today_coin_count  = today_coin_count
         @today_total_price = today_total_price
+        @today_trade_members_count = today_trade_members_count
       end
 
       private
+
+        def init_query
+          start_time = Time.now.beginning_of_day
+          end_time  = Time.now
+
+          Trade.search(created_at_gteq: start_time, created_at_lteq: end_time).result
+        end
+
         def today_trade_count
-          TODAY_TRADE_RESULT.count
+          init_query.count
         end
 
         def today_coin_count
-          TODAY_TRADE_RESULT.sum(:volume)
+          init_query.sum(:volume)
         end
 
         def today_total_price
-          TODAY_TRADE_RESULT.sum("(volume * price)")
+          init_query.sum("(volume * price)")
+        end
+
+        def today_trade_members_count
+          ask_members = init_query.pluck("DISTINCT(ask_member_id)")
+          bid_members = init_query.pluck("DISTINCT(bid_member_id)")
+
+          (ask_members | bid_members).count
         end
     end
   end
