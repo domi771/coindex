05faac71a358fafba3267d02852e1c58e146b9d1
domi771 <dpiatti771@gmail.com>
2014-08-28 15:21:07 +0200

    small fixes

--- app/controllers/private/api_tokens_controller.rb
+++ app/controllers/private/api_tokens_controller.rb
@@ -1,7 +1,7 @@
 module Private
   class APITokensController < BaseController
     before_action :auth_activated!
-    before_action :auth_verified!
+    # before_action :auth_verified!
     before_action :two_factor_activated!
 
     def index
