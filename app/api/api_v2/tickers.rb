6b07a877c6c9c16493d59e54fe7ab59344a704e3
domi771 <dpiatti771@gmail.com>
2014-09-09 11:20:11 +0200

    merge with main branch

--- app/api/api_v2/tickers.rb
+++ app/api/api_v2/tickers.rb
@@ -15,37 +15,8 @@ module APIv2
       use :market
     end
     get "/tickers/:market" do
-<<<<<<< HEAD
-      ticker = Global[params[:market]].ticker
-
-      { at: ticker[:at],
-        ticker: {
-          buy: ticker[:buy],
-          sell: ticker[:sell],
-          low: ticker[:low],
-          high: ticker[:high],
-          last: ticker[:last],
-          vol: ticker[:volume],
-          change: ticker[:change]
-        }
-      }
-=======
       format_ticker Global[params[:market]].ticker
->>>>>>> upstream/master
     end
-
-    get "/tickers_all" do
-
-    #a1 = ["some", "thing"]
-    #a2 = ["another", "thing"]
-
-    a1 = ["ltcbtc"]
-    a2 = json
-
-    a1.concat a2
-    present a1
-    end
-
 
   end
 end
